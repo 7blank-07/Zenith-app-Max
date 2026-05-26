313623c1b965b09836328583a61bc677d131c62a
Vaibhav Sonkamble <0blank07@users.noreply.github.com>
2026-05-26 15:37:00 +0530

    favicon done

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,7 @@ export const metadata = {
   description: 'Zenith FC Mobile tools and database',
   icons: {
     icon: [
+      { url: '/favicon.ico' },
       { url: '/icon.png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
